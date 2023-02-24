aa66a40d799032a3b448df5282bccdfb3d92a08b
Natalyae <ngk.s@mail.ru>
2023-02-24 23:01:27 +0100

    Add code hw_02

--- hw_02/Program.cs
+++ hw_02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void SumNumber(int number){
+    int sNumber = 0;
+    int num = number;
+    while (num > 0){
+        sNumber = sNumber + num%10;
+        num = num/10;
+    }
+    Console.WriteLine(sNumber);
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SumNumber(number);
