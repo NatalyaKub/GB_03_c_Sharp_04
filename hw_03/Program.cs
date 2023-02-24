2201ea5d0f655bcc81fea47ca1ef217a1ee4a827
Natalyae <ngk.s@mail.ru>
2023-02-24 23:49:15 +0100

    Add code hw_03

--- hw_03/Program.cs
+++ hw_03/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void ArraySet (int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    Console.WriteLine("Задаем массив:");
+    while (index < length){
+        //collection[index] = Convert.ToInt32(Console.ReadLine());
+        collection[index] = new Random().Next(1,100);
+        index++;
+    }
+    Console.WriteLine();
+}
+
+void ArrayPrint(int[] col){
+    int count = col.Length;
+    int index = 0;
+    while (index < count){
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int n = 8;
+int[] array = new int[n];
+
+ArraySet(array);
+ArrayPrint(array);
